91fad7b51d98335dff48f87daa5a033149eb36bf
Ed Power <epower@cox.net>
2023-03-03 11:56:17 -0800

    Filled in customer combo box

--- LogViewerWpf/LogViewerWpf/MainWindow.xaml.cs
+++ LogViewerWpf/LogViewerWpf/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
 namespace LogViewerWpf
 {
@@ -20,9 +23,29 @@ namespace LogViewerWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        HttpClient client;
+
         public MainWindow()
         {
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
             InitializeComponent();
+            var customers = GetCustomers();
+            comboBoxCustomers.Items.Add("All");
+            foreach ( var customer in customers )
+            {
+                comboBoxCustomers.Items.Add( customer );
+            }
+            comboBoxCustomers.SelectedIndex = 0;
+        }
+
+        private List<string> GetCustomers()
+        {
+            var url = "https://localhost:7297/customers";
+            var response = client.GetAsync(url).Result;
+            var content = response.Content.ReadAsStreamAsync();
+            var contentString = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<List<string>>(contentString) ?? new List<string>();
         }
     }
 }
